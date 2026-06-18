a140598c4e18ac01498400678fe3afcebe785c42
abdulahad <abdulahad.devops@gmail.com>
2026-06-18 13:10:08 +0500

    user and group complete

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "us-east-1"
+}
+# IAM User Creation
+resource "aws_iam_user" "user" {
+  name = "ahad-tf"
+
+}
+#IAM group creation
+resource "aws_iam_group" "group" {
+  name = "devops-group"
+
+}
+# ADD user to group
+resource "aws_iam_user_group_membership" "membership" {
+  user   = aws_iam_user.user.name
+  groups = [aws_iam_group.group.name]
+}
+# Attach policy to group
+resource "aws_iam_group_policy_attachment" "group_policy_attachment" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+  ])
+  group      = aws_iam_group.group.name
+  policy_arn = each.value
+}
+#IAM ROle Creation
+resource "aws_iam_role" "Ec2-role" {
+  name = "EC2-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+}
