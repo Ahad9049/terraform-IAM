b6cb7446ce10918f383dbd1cf7b94f45c9ab9fb0
abdulahad <abdulahad.devops@gmail.com>
2026-06-22 14:46:56 +0500

    show vlues or accces key for a user

---
+++ ouput.tf
@@ -0,0 +1,17 @@
+output "iam_user_name" {
+  value = aws_iam_user.user.name
+}
+output "iam_group_name" {
+  value = aws_iam_group.group.name
+}
+output "aws_iam_instance_profile_name" {
+  value = aws_iam_instance_profile.instance_profile.name
+}
+output "access_key_id" {
+  value = aws_iam_access_key.access_key.id
+  sensitive = true
+}
+output "secret_acceess_key" {
+  value = aws_iam_access_key.access_key.secret
+  sensitive = true
+}
