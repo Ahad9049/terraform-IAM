7160c39bf068e82fe30f055dbf9ebcd5df8b588a
abdulahad <abdulahad.devops@gmail.com>
2026-06-23 16:16:05 +0500

    prod iam compete

---
+++ environments/prod/main.tf
@@ -0,0 +1,8 @@
+module "iam" {
+  source       = "../../module/iam"
+  user_name    = var.iam_user_name
+  group_name   = var.iam_group_name
+  role_name    = var.iam_role_name
+  iam_policies = var.iam_policies
+
+}
